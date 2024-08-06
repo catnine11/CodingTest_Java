773d8a53290249a1aefcaf98085d843fb2ac2575
catnine11 <134343781+catnine11@users.noreply.github.com>
2024-08-06 20:38:23 +0900

    [Bronze III] Title: 최소, 최대, Time: 1504 ms, Memory: 317776 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -3,15 +3,29 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
         int[] nums = new int[N];
         for (int i = 0; i < N; i++) {
             nums[i] = sc.nextInt();
         }
-        Arrays.sort(nums);
+//        Arrays.sort(nums);
 
-        System.out.printf("%d %d", nums[0], nums[N-1]);
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int i = 0; i < N; i++){
+            if(nums[i] < min){
+                min = nums[i];
+            }
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+
+
+        System.out.printf("%d %d", min, max);
+        sc.close();
     }
 }
