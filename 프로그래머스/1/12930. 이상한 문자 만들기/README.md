9902c22fd032c350c37ef4af739f033d95a14c38
catnine11 <134343781+catnine11@users.noreply.github.com>
2024-08-12 16:27:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.33 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930#) 
 
 ### 성능 요약
 
-메모리: 92.2 MB, 시간: 27.49 ms
+메모리: 67.7 MB, 시간: 0.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 12일 16:13:13
+2024년 08월 12일 16:27:13
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -6,23 +6,19 @@ class Solution {
 
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
-//            StringBuilder sb = new StringBuilder();
+           StringBuilder sb = new StringBuilder();
             String convert = "";
 
             for (int j = 0; j < word.length(); j++) {
                 char c = word.charAt(j);
                 if(j % 2 == 0) {
-//                    sb.append(Character.toUpperCase(c));
-                    char up = Character.toUpperCase(c);
-                    convert += up;
+                   sb.append(Character.toUpperCase(c));
                 } else {
-//                    sb.append(Character.toLowerCase(c));
-                    char down = Character.toLowerCase(c);
-                    convert += down;
+                   sb.append(Character.toLowerCase(c));
                 }
             }
 
-            result.append(convert);
+            result.append(sb);
 
             if (i < words.length - 1) {
                 result.append(" ");
