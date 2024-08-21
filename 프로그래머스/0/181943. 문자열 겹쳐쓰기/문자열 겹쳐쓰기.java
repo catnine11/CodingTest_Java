f0997c38fd7431e44bc4ff903fa82620a6cbbc14
catnine11 <134343781+catnine11@users.noreply.github.com>
2024-08-21 17:42:38 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.09 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,13 +1,14 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
-        String front = my_string.substring(0, s);
-        if(my_string.length() <= s+overwrite_string.length()){
-            answer = front + overwrite_string;
-        } else{
-        String after = my_string.substring(s+overwrite_string.length());
-        answer = front + overwrite_string + after;
-        }
-        return answer;
+
+        // String answer = "";
+        // String front = my_string.substring(0, s);
+        // String after = my_string.substring(s+overwrite_string.length());
+        // answer = front + overwrite_string + after;
+        // return answer;
+        
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.replace(s, s+overwrite_string.length(), overwrite_string);
+        return sb.toString();
     }
 }
